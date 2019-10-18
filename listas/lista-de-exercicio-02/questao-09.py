0470cde23713d0c83b871eb8546e7dec32eefa0e
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-18 16:42:20 -0300

    Enviando a questão 9

---
+++ listas/lista-de-exercicio-02/questao-09.py
@@ -0,0 +1,3 @@
+temperatura = float(input('Digite a temperatura em graus Fahrenheit:'))
+mudando_a_temperatura = (5*(temperatura-32)/9)
+print('A temperatura em graus Celsius é:{:.0f}'.format(mudando_a_temperatura))
