c36139b3f4fc642250cd7d74fa6195c09073e3c8
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-18 16:21:57 -0300

    Enviando a questão 7

---
+++ listas/lista-de-exercicio-02/questao-07.py
@@ -0,0 +1,5 @@
+base = float(input('Digite a base do quadrado:'))
+altura = float(input('Digite a altura do quadrado:'))
+calculo_da_área_do_quadrado = base*altura
+print('Area do quadrado:{:.2f}'.format(calculo_da_área_do_quadrado))
+print('O dobro da área do quadrado é igual a:{:.2f}'.format(calculo_da_área_do_quadrado*2))
