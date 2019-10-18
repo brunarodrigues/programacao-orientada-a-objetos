6cf050418dfce20741734ba3536d9f7963534e72
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-18 16:35:18 -0300

    Enviando a questão 8

---
+++ listas/lista-de-exercicio-02/questao-08.py
@@ -0,0 +1,4 @@
+ganho_por_hora = float(input('Quanto você ganha por hora:'))
+horas_trabalhadas_no_mês = float(input('Horas trabalhadas no mês:'))
+calculo = ganho_por_hora*horas_trabalhadas_no_mês
+print('O salário no referido mês é igual a:{:.2f}'.format(calculo))
