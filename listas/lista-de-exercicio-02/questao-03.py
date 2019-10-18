f622b3a51255d0f38489f016e28edcb86df6b67b
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-18 15:40:24 -0300

    Enviando questões de 1 a 4

---
+++ listas/lista-de-exercicio-02/questao-03.py
@@ -0,0 +1,5 @@
+
+primeiro_numero = int(input('digite um numero:'))
+segundo_numero = int(input('digite outro numero:'))
+soma = primeiro_numero + segundo_numero
+print('A soma entre os dois numeros é: {}'.format(soma))
---
+++ listas/lista-de-exercicio-02/questao-04.py
@@ -0,0 +1,12 @@
+
+
+nota_bimestral_1 = float(input('Digite a primeira nota:')) 
+nota_bimestral_2 = float(input('Digite a segunda nota:'))
+nota_bimestral_3 = float(input('Digite a terceira nota:'))
+
+nota_bimestral_4 = float(input('Digite a quarta nota:'))
+
+media = (nota_bimestral_1 + nota_bimestral_2 + nota_bimestral_3 + nota_bimestral_4)/4
+print('Sua média bimestral é:{:.2f}'.format(media))
+
+
