aa1832d48cb58ea8d8c0b9df46e15d6d77eb32f3
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-24 16:38:29 -0300

    adicionando a questão 10

---
+++ listas/lista-de-exercicio-02/questao-10.py
@@ -0,0 +1,5 @@
+temperatura_em_celsius = float(input('temperatura em Celsius:'))
+calculo1 = temperatura_em_celsius/5
+calculo2 = calculo1*9
+calculo3 = calculo2 + 32
+print('Graus em Fahrenheit: {:.2f}'.format(calculo3))
