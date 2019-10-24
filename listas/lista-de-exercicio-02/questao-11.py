0fe6d60e41a2022862b93c52e128678cbe5b118d
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-24 16:53:39 -0300

    Adicionando a questao 11

---
+++ listas/lista-de-exercicio-02/questao-11.py
@@ -0,0 +1,9 @@
+numero1= int(input('Digite um numero:'))
+numero2= int(input('Digite um numero:'))
+numero_real = float(input('Digite um numero:'))
+produto=(numero1*2)*(numero2/2)
+soma=(numero1*3)+(numero_real)
+exponenciacao = numero_real**3
+print(produto)
+print(soma)
+print(exponenciacao)
