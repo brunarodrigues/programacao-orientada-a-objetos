297ac0d564d9ee7b000ef47ebda2e9a686d19b70
Bruna Amorim <brunaamorim5678@gmail.com>
2019-10-24 21:09:05 -0300

    Enviando a questão 01

---
+++ listas/lista-de-exercicio-03/questão-01.py
@@ -0,0 +1,6 @@
+primeiro_numero = int(input('Digite um número:'))
+segundo_numero = int(input('Digite um número:'))
+if (primeiro_numero>segundo_numero):
+  print('primeiro numero = maior')
+else:
+  print('segundo numero = maior')
